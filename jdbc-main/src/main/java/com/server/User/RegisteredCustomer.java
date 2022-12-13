bf204a413af790c63321466a724e19785d47db8c
furkanguzelant <72248390+furkanguzelant@users.noreply.github.com>
2022-12-13 13:02:46 +0300

    RegisteredCustomer implemented

---
+++ jdbc-main/src/main/java/com/server/User/RegisteredCustomer.java
@@ -0,0 +1,48 @@
+package com.server.User;
+
+import java.time.LocalDate;
+
+public class RegisteredCustomer extends User {
+
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+    public RegisteredCustomer() {}
+    public RegisteredCustomer(String email, String password, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public RegisteredCustomer(String name, LocalDate birthDate, String email, String password, String phoneNumber) {
+        super(name, birthDate);
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- jdbc-main/src/main/resources/db/migration/V1__InitTables.sql
+++ jdbc-main/src/main/resources/db/migration/V1__InitTables.sql
@@ -1,44 +1,49 @@
-CREATE TABLE users (
-                      userID SERIAL NOT NULL,
-                      name varchar(64),
-                      birthDate date,
-                      PRIMARY KEY(userID)
+CREATE TABLE users
+(
+    userID    SERIAL NOT NULL,
+    name      varchar(64),
+    birthDate date,
+    PRIMARY KEY (userID)
 );
 
-CREATE TABLE customer (
-                          userID int NOT NULL,
-                          PRIMARY KEY (userID),
-                          FOREIGN KEY (userID ) REFERENCES users(userID)
+CREATE TABLE customer
+(
+    userID int NOT NULL,
+    PRIMARY KEY (userID),
+    FOREIGN KEY (userID) REFERENCES users (userID)
 );
 
-CREATE TABLE staff (
-                       userID int NOT NULL,
-                       email varchar(320) NOT NULL,
-                       password varchar(64) NOT NULL,
-                       phoneNumber varchar(15) NOT NULL,
-                       salary int,
-                       PRIMARY KEY (userID),
-                       FOREIGN KEY (userID) REFERENCES users(userID),
-                       UNIQUE (email),
-                       UNIQUE (phoneNumber)
+CREATE TABLE staff
+(
+    userID      int          NOT NULL,
+    email       varchar(320) NOT NULL,
+    password    varchar(64)  NOT NULL,
+    phoneNumber varchar(15)  NOT NULL,
+    salary      int,
+    PRIMARY KEY (userID),
+    FOREIGN KEY (userID) REFERENCES users (userID),
+    UNIQUE (email),
+    UNIQUE (phoneNumber)
 );
 
-CREATE TABLE admin(
-                      userID int,
-                      PRIMARY KEY (userID),
-                      FOREIGN KEY (userID) REFERENCES users(userID)
+CREATE TABLE admin
+(
+    userID int,
+    PRIMARY KEY (userID),
+    FOREIGN KEY (userID) REFERENCES users (userID)
 );
 
 
-create table registeredCustomer (
-                                    userID      			int,
-                                    email            			varchar(320),
-                                    password      			varchar(64),
-                                    phoneNumber   		varchar(15),
-                                    PRIMARY KEY (userID),
-                                    FOREIGN KEY (userID) REFERENCES customer(userID),
-                                    UNIQUE (email),
-                                    UNIQUE (phoneNumber)
+create table registeredCustomer
+(
+    userID      int,
+    email       varchar(320),
+    password    varchar(64),
+    phoneNumber varchar(15),
+    PRIMARY KEY (userID),
+    FOREIGN KEY (userID) REFERENCES customer (userID),
+    UNIQUE (email),
+    UNIQUE (phoneNumber)
 );
 
 
