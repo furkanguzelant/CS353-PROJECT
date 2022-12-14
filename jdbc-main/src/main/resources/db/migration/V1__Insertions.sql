9c8886dfad773bdb9151c02f8a0695ad92666dd7
furkanguzelant <72248390+furkanguzelant@users.noreply.github.com>
2022-12-14 17:33:54 +0300

    some logistic units inserted

--- jdbc-main/src/main/resources/db/migration/V1__Insertions.sql
+++ jdbc-main/src/main/resources/db/migration/V1__Insertions.sql
@@ -1,4 +1,31 @@
 
 
-INSERT INTO logisticUnit
-values("Bilkent Branch", )
+INSERT INTO logisticUnit(name, addressid)
+values('Karsiyaka Branch', 1);
+
+INSERT INTO logisticUnit(name, addressid)
+values('Samsun Distribution Center', 2);
+
+INSERT INTO distributioncenter
+values(2);
+
+INSERT INTO logisticUnit(name, addressid)
+values('Yeniköy Branch', 3);
+
+INSERT INTO logisticUnit(name, addressid)
+values('Samsun Yeşilköy Branch', 4);
+
+INSERT INTO branch
+values(4, 2);
+
+INSERT INTO logisticUnit(name, addressid)
+values('İzmir Distribution Center', 8);
+
+INSERT INTO distributioncenter
+values(5);
+
+INSERT INTO branch
+values(1, 5);
+
+SELECT *
+FROM logisticunit natural join address
