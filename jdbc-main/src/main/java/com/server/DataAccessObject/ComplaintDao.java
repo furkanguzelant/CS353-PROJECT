894fff2ef15288b00a574eea9995f1e21b537dd7
bengisubuket <bengisubuket@icloud.com>
2022-12-16 06:11:25 +0300

    Minor mistakes in Package dao and services are corrected

---
+++ jdbc-main/src/main/java/com/server/DataAccessObject/ComplaintDao.java
@@ -0,0 +1,18 @@
+package com.server.DataAccessObject;
+
+import com.server.ModelClass.Complaint;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ComplaintDao {
+
+    void insertComplaint(Complaint complaint);
+
+    List<Complaint> selectAllComplaints();
+
+    Optional<Complaint> getComplaintById(int complaintID);
+
+    Optional<Complaint> getComplaintByPackageId(int packageID);
+
+}
