5562822e8319bc6140f91e1b024972d652f8a085
furkanguzelant <72248390+furkanguzelant@users.noreply.github.com>
2022-12-18 03:44:40 +0300

    LogisticUnitController updated

---
+++ jdbc-main/src/main/java/com/server/ControllerClass/LogisticUnitController.java
@@ -0,0 +1,32 @@
+package com.server.ControllerClass;
+
+import com.server.ModelClass.LogisticUnits.LogisticUnit;
+import com.server.ServiceClass.LogisticUnitService;
+import com.server.ServiceClass.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin
+@RestController
+@RequestMapping(path = "api/logisticUnit")
+public class LogisticUnitController {
+
+    private final LogisticUnitService logisticUnitService;
+
+    public LogisticUnitController(LogisticUnitService logisticUnitService) {
+        this.logisticUnitService = logisticUnitService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getLogisticUnits() {
+        List<LogisticUnit> logisticUnitList = logisticUnitService.getLogisticUnits();
+        return new ResponseEntity<>(Map.of("statusMessage", "Successfully fetched.","logisticUnitList", logisticUnitList), HttpStatus.OK);
+    }
+}
